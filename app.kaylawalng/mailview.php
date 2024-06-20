fc76bc179841a87a8b61c851501fa142c6c82cd1
Damilare <sdesparkflux@gmail.com>
2024-06-20 14:11:30 +0100

    Mail template

--- app.kaylawalng/mailview.php
+++ app.kaylawalng/mailview.php
@@ -1,27 +1,67 @@
-<!DOCTYPE html>
-<html lang="en">
-    <body 
-        style="
-            margin: 0;
-            padding: 0;
-            -moz-box-sizing: border-box;
-            -webkit-box-sizing: border-box;
-            box-sizing: border-box;
-            font-family: 'Rubik', 'san-serif';">
-        <main 
-            style="
-                width: 100%;
-                height: auto;
-                background-color: #fff;
-                padding: 5%;">
+<html>
+<body style="@import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap'); font-family: 'Rubik', 'San-serif';">
+    <div style="width: 100%; height: 100vh;margin: 0;
+        padding: 10%;
+        -moz-box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        /* display: flex; */
+        justify-content: center;">
+        <div style="width: 100%; height: 400px; background: url('assets/images/img2.png');background-position: center; background-size: cover;
+         background-repeat: no-repeat; border-radius: 10px;"></div>
 
-            <section>
-                <div style="height: 50px; width: 100%; display: flex; flex-direction: row; flex-wrap:nowrap; justify-content:space-between;">
-                    <h1 style="width: 50%;font-family: 'Rubik', 'san-serif';">Onboarding in Progress</h1>
-                    <img src="http://localhost/kaylawalng/assets/images/kayl%20logo%20red&black.png" style="height: 100%">
-                </div>
-            </section>
+        <div style="width: 100%;">
+            <p style="font-size: 25pt; margin: 5%">Hello @fullname</p>
+        </div>
 
-        </main>
-    </body>
+        <div style="width: 100%; margin: 5%">
+            <p style="font-size: 25pt; ">You are almost done! One more step and you're onboard.</p>
+        </div>
+
+        <div style="width: 100%;margin: 5%">
+            <p style="font-size: 25pt; ">Kindly copy the verification code below</p>
+            <p style="font-size: 25pt;">Verification code is VCODE.</p>
+        </div>
+
+        <div style="width: 100%; text-align: center">
+            <h1 style="font-size: 45pt; margin: 5%; color: #8e0000">VCODE</h1>
+        </div>
+        <hr>
+        <div style="width: 100%; margin: 5%;">
+            <p style="font-size: 25pt;">With KAYLET (Kaylawal Wallet) you can now:</p>
+            <div style="width: 70%; display: flex; flex-direction: row; flex-wrap: no-wrap; align-items: center; margin-bottom: 10px;">
+                <img style="width: 40px; height: 40px; margin-right: 20px;" src="assets/images/bitcoinMail.png" alt="">
+                <p style="font-size: 18pt;">Convert your bitcoin and all other crypto currencies to instant cash.</p>
+            </div>
+
+            <div style="width: 70%; display: flex; flex-direction: row; flex-wrap: no-wrap; align-items: center; margin-bottom: 10px;">
+                <img style="width: 40px; height: 40px; margin-right: 20px;" src="assets/images/gift-voucher.png">
+                <p style="font-size: 18pt;">Convert your gift cards to cash.</p>
+            </div>
+
+            <div style="width: 70%; display: flex; flex-direction: row; flex-wrap: no-wrap; align-items: center; margin-bottom: 10px;">
+                <img style="width: 40px; height: 40px; margin-right: 20px;" src="https://ci3.googleusercontent.com/meips/ADKq_NZeND5jKEBkWbaKegrpz1eUzC-llu-zTPGUrdX3ie3WTQmemDZmoVLRtnjTlCojW4SfOyj5OAu92hSuRyGRrHEkgc5l-kvdFcWr6EEzUwsAzBzRhNEJDT2253xckETvgBXRQTlf3bXBlu-bG4Ar3Rvys0aun6xHc47c-3pdCXucCLbvqE_GC_Fk_Q=s0-d-e1-ft#http://cdn.mcauto-images-production.sendgrid.net/2d4f01da7927a96a/0a282ae3-ccf1-4bd0-8164-4165bc929362/2160x2160.jpg" alt="">
+                <p style="font-size: 18pt;">Pay your Utility bills with ease.</p>
+            </div>
+
+            <div style="width: 70%; display: flex; flex-direction: row; flex-wrap: no-wrap; align-items: center; margin-bottom: 10px;">
+                <img style="width: 40px; height: 40px; margin-right: 20px;" src="assets/images/iphone.png">
+                <p style="font-size: 18pt;">Top up your airtime and renew your internet subscription.</p>
+            </div>
+        </div>
+
+        <hr>
+
+        <div style="width: 100%; border-radius: 10px; padding-top: 5px; padding-bottom: 1px; margin-top: 5%; background: #ffeded;">
+            <p style="margin: 5%; font-size: 14pt;">To learn more about our products and services, <a href="kaylawalng.com">click here</a> or contact us 
+                at <a href="mailto: support@kaylawalng.com">support@kaylawalng.com</a> or send us a WhatsApp message at <a href="http://wa.me/08145033094">+234 814 5033 094</a></p>
+        </div><br>
+        
+        <div style="display: flex; flex-direction: row;">
+            <a href=""><img style="width: 40px; height: 40px; margin-right: 20px;" src="assets/images/facebook.png"></a>
+            <a href=""><img style="width: 40px; height: 40px; margin-right: 20px;" src="assets/images/instagram.png"></a>
+            <a href=""><img style="width: 40px; height: 40px; margin-right: 20px;" src="assets/images/twitter.png"></a>
+        </div><br><br><br><br><br>
+    </div>
+</body>
 </html>
