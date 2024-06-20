606e6546afefe672c9506352a433160266a0b2ca
Damilare <sdesparkflux@gmail.com>
2024-06-20 17:28:19 +0100

    Database Connection Set

--- config/dbcon.php
+++ config/dbcon.php
@@ -1,14 +1,14 @@
 <?php
 
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "kaylawal";
-
     // $host = "localhost";
-    // $username = "alphonzt_fibe";
-    // $password = "Trodpen2022*";
-    // $database = "alphonzt_fibe";
+    // $username = "root";
+    // $password = "";
+    // $database = "kaylawal";
+
+    $host = "localhost";
+    $username = "kaylxpdy_kaylawalUserbility";
+    $password = "Trodpen2022*";
+    $database = "kaylxpdy_kaylawal";
 
     $con = mysqli_connect($host, $username, $password, $database);
 
